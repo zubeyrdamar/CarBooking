018bf42fb8923ceabe132cb48fd732f97f03e772
zubeyr <zubeyrdamar@outlook.com>
2023-12-06 16:54:36 +0300

    Customers

---
+++ App/CustomerApi/Services/CustomerService.cs
@@ -0,0 +1,31 @@
+﻿using CustomerApi.Data;
+using CustomerApi.Interfaces;
+using CustomerApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerApi.Services
+{
+    public class CustomerService : ICustomer
+    {
+
+        private readonly ApiDbContext _context;
+        public CustomerService()
+        {
+            _context = new ApiDbContext();
+        }
+
+        public async Task Create(Customer customer)
+        {
+            var tempVehicle = await _context.Vehicles.FirstOrDefaultAsync(o => o.Id == customer.Id);
+            if (tempVehicle == null && customer.Vehicle != null)
+            {
+                await _context.Vehicles.AddAsync(customer.Vehicle);
+                await _context.SaveChangesAsync();
+            }
+
+            customer.Vehicle = null;
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
